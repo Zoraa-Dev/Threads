712dfb17d4e894d0ab843c9851840f4c35e2b18f
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2025-08-12 00:52:54 +0700

    Add files via upload

---
+++ Menu/Data/data.py
@@ -0,0 +1,11 @@
+# Zoraa Dev/Dev Knowing
+
+try:
+    import requests
+    import json
+    import re
+    import time
+    import os
+    import sys
+except (ImportError, ValueError) as e:
+    exit(f'[•] Module Error: {str(e).title()}')
